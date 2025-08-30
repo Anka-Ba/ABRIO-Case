71765fc524ad64b3a028a956a51fbc6688b260cd
Ann-Kathrin Barth <ann-kathrin.barth@student.hs-rm.de>
2025-08-30 12:46:09 +0200

    feat: only the product cards matching the filter criteria appear

--- frontend/src/components/ProductList/ProductList.tsx
+++ frontend/src/components/ProductList/ProductList.tsx
@@ -1,4 +1,5 @@
 import type { Product } from "../../types/types";
+import { useFilterOptions } from "../Filter/FilterReducer";
 import { ProductCard } from "../ProductCard/ProductCard";
 import styles from "./ProductList.module.css";
 
@@ -7,12 +8,26 @@ type ProductListProps = {
 };
 
 export const ProductList = ({ products }: ProductListProps) => {
+  const currentFilterCriteria = useFilterOptions();
+  console.log(currentFilterCriteria);
   return (
     <div className={styles.listContainer}>
       {/* Display a product card for each product */}
-      {products.map((product) => (
-        <ProductCard product={product} key={product.id} />
-      ))}
+      {products
+        .filter(
+          (product) =>
+            currentFilterCriteria.colors.includes(product.color) ||
+            currentFilterCriteria.colors.length === 0,
+        )
+        .filter(
+          (product) =>
+            currentFilterCriteria.categories.some((category) =>
+              product.categories.includes(category),
+            ) || currentFilterCriteria.categories.length === 0,
+        )
+        .map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
     </div>
   );
 };
